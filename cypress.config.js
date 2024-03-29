0fe47e07fc13cd56fc5212c08b3564233215e3bf
sp.matheus.msilva@gmail.com <sp.matheus.msilva@gmail.com>
2024-03-29 15:51:17 -0300

    fixing reporters

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   reporter: 'cypress-mochawesome-reporter',
